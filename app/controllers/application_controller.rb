b1e7eea37a67ecea2e5ae53ca4bc5413363fe7e3
Lorena Cueva <lorenacueva@gmail.com>
2023-01-27 20:09:30 -0800

    Adds User Model, User Controller, and create route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  
+  def render_not_found(error)
+    render json: {errors: error.record.errors.full_messages}, status: :not_found
+  end
+
+  def render_unprocessable_entity(error)
+    render json: {errors: error.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
 end
